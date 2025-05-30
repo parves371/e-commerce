b572a827ab46897c97fe44d10752b365bf48ca94
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2025-05-30 20:55:48 +0600

    Refactor price filtering logic in productsRouter for improved readability and maintainability.

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -14,16 +14,18 @@ export const productsRouter = createTRPCRouter({
     )
     .query(async ({ ctx, input }) => {
       const where: Where = {};
-        if (input.minPrice) {
-          where.price = {
-            greater_than_equal: input.minPrice,
-          };
-        }
-        if (input.maxPrice) {
-          where.price = {
-            less_than_equal: input.maxPrice,
-          };
-        }
+      if (input.minPrice) {
+        where.price = {
+          ...where.price,
+          greater_than_equal: input.minPrice,
+        };
+      }
+      if (input.maxPrice) {
+        where.price = {
+          ...where.price,
+          less_than_equal: input.maxPrice,
+        };
+      }
 
       if (input.category) {
         const cataegoriesData = await ctx.db.find({
