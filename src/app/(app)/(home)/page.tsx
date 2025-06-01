c3efaea8fbeaf7a016cc512240ff3e992ba8ca4d
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2025-06-01 13:36:38 +0600

    Refactor page component; update DEFAULT_LIMIT constant to 8; implement async page function with product filters and prefetching for improved performance.

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,11 +1,30 @@
-"use client";
+import { DEFAULT_LIMIT } from "@/constants";
+import { loadProductFilters } from "@/modules/products/search-params";
+import { ProductListView } from "@/modules/products/ui/view/product-list-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { SearchParams } from "nuqs/server";
 
-import { useQuery } from "@tanstack/react-query";
-import { useTRPC } from "@/trpc/client";
-
-export default function Home() {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.auth.session.queryOptions());
-
-  return <div>{JSON.stringify(data?.user)}</div>;
+interface Props {
+  searchParams: Promise<SearchParams>;
 }
+
+const page = async ({ searchParams }: Props) => {
+  const filters = await loadProductFilters(searchParams);
+
+  const queryClient = getQueryClient();
+  void queryClient.prefetchInfiniteQuery(
+    trpc.products.getMany.infiniteQueryOptions({
+      ...filters,
+      limit: DEFAULT_LIMIT,
+    })
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <ProductListView />
+    </HydrationBoundary>
+  );
+};
+
+export default page;
