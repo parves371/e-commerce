d77a81b0a867d7eecaca57d1ebc877d4a80b29d3
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2025-06-11 15:44:56 +0600

    Refactor generateTenantUrl function to improve clarity and add subdomain routing condition

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,14 +6,18 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function generateTenantUrl(tenantSlug: string) {
-  // in development use normal routing
-  if (process.env.NODE_ENV === "development") {
+  const isDevolepment = process.env.NODE_ENV === "development";
+  const isSubdomainRoutingEnabled = Boolean(
+    process.env.NEXT_PUBLIC_EANBLE_SUBDOMAIN_ROUTING!
+  );
+
+  // in development use normal routing or if subdomain routing is disabled, use normal routing
+  if (isDevolepment || !isSubdomainRoutingEnabled) {
     return `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${tenantSlug}`;
   }
 
   const protocol = "https";
   const domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN!;
-
 
   // in production use subdomain routing
   // https://parves.vendspace.com
