baef91ca13bf272ddfb07b07cef87a897e1bcd9a
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2025-06-12 11:59:03 +0600

    Add storage-vercel-blob package and configure Vercel Blob storage in payload.config.ts

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -18,6 +18,7 @@ import { Users } from "./collections/Users";
 import { isSuperAdmin } from "./lib/access";
 import { Config } from "./payload-types";
 import { Orders } from "./collections/bug/Orders";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -62,6 +63,12 @@ export default buildConfig({
       },
       userHasAccessToAllTenants: (user) => isSuperAdmin(user),
     }),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 });
